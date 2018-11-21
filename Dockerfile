cc7703e80ce65f62b230fec5d3c7d174b0472d3a
Andy the Mother Fucking Best <atetnowski@gmail.com>
2018-11-20 17:43:54 -0800

    Puts things in the correct files. Fixed my stupidity

--- Dockerfile
+++ Dockerfile
@@ -1,32 +1,7 @@
-#!/bin/bash
+FROM redis:5
 
-# This file is a modified version of create-cluster.sh from the official Redis repo
-# Original file can be found here https://github.com/antirez/redis/blob/unstable/utils/create-cluster/create-cluster
+COPY create-cluster.sh create-cluster.sh
 
-# Settings
-START_PORT=6379
-PORT=$START_PORT
-TIMEOUT=2000
-NODES=6
-REPLICAS=1
+EXPOSE 6379-6381
 
-# Computed vars
-ENDPORT=$((PORT+NODES))
-HOSTS=""
-
-# Loop to create individual redises
-while [ $((PORT < ENDPORT)) != "0" ]; do
-    echo "Starting $PORT"
-    redis-server --port $PORT --cluster-enabled yes --cluster-config-file nodes-${PORT}.conf --cluster-node-timeout $TIMEOUT --appendonly yes --appendfilename appendonly-${PORT}.aof --dbfilename dump-${PORT}.rdb --logfile ${PORT}.log --daemonize yes
-    HOSTS="$HOSTS 127.0.0.1:$PORT"
-    PORT=$((PORT+1))
-done
-
-# Uses redis cli to link the redis created above into a cluster
-echo "yes" | redis-cli --cluster create $HOSTS --cluster-replicas $REPLICAS
-
-# Prints some info about the cluster
-redis-cli --cluster check 127.0.0.1:$START_PORT
-
-# Tails logs for all the redises
-tail -f *.log
+ENTRYPOINT ["./create-cluster.sh"]
--- create-cluster.sh
+++ create-cluster.sh
@@ -1,7 +1,32 @@
-FROM redis:5
+#!/bin/bash
 
-COPY create-cluster.sh create-cluster.sh
+# This file is a modified version of create-cluster.sh from the official Redis repo
+# Original file can be found here https://github.com/antirez/redis/blob/unstable/utils/create-cluster/create-cluster
 
-EXPOSE 6379-6381
+# Settings
+START_PORT=6379
+PORT=$START_PORT
+TIMEOUT=2000
+NODES=6
+REPLICAS=1
 
-ENTRYPOINT ["./create-cluster.sh"]
+# Computed vars
+ENDPORT=$((PORT+NODES))
+HOSTS=""
+
+# Loop to create individual redises
+while [ $((PORT < ENDPORT)) != "0" ]; do
+    echo "Starting $PORT"
+    redis-server --port $PORT --cluster-enabled yes --cluster-config-file nodes-${PORT}.conf --cluster-node-timeout $TIMEOUT --appendonly yes --appendfilename appendonly-${PORT}.aof --dbfilename dump-${PORT}.rdb --logfile ${PORT}.log --daemonize yes
+    HOSTS="$HOSTS 127.0.0.1:$PORT"
+    PORT=$((PORT+1))
+done
+
+# Uses redis cli to link the redis created above into a cluster
+echo "yes" | redis-cli --cluster create $HOSTS --cluster-replicas $REPLICAS
+
+# Prints some info about the cluster
+redis-cli --cluster check 127.0.0.1:$START_PORT
+
+# Tails logs for all the redises
+tail -f *.log
